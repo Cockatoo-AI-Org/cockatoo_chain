b6b49453e5493659b98311fad249273b9ea0ddcd
johnklee <puremonkey2001@yahoo.com.tw>
2025-03-22 11:04:14 +0000

    Initializes `model_a` package.

---
+++ cockatoo_chain/utils/wrapper.py
@@ -0,0 +1,153 @@
+"""Wrapper for pre-defined API interfaces of model A, B and C packages."""
+
+import dataclasses
+import enum
+from typing import Protocol
+
+
+@dataclasses.dataclass
+class Audio2TextData:
+  """Audio to text result or known as Model A's output."""
+  text: str
+  spent_time_sec: float
+  audio_file_path: str
+
+
+@dataclasses.dataclass
+class Text2AudioData:
+  """Text to audio result or known as Model C's output."""
+  text: str
+  spent_time_sec: float
+  generated_audio_file_path: str
+
+
+@enum.unique
+class LangEnum(enum.Enum):
+  """Supported language enumeration."""
+
+  en = 0
+  cn = 1
+  multi_lang = -1
+
+  @classmethod
+  def from_str(cls, lang_str: str):
+    """Transforms input string into corresponding `LangEnum`.
+
+    Rasies:
+      ValueError: Invalid input string.
+    """
+    for supported_lang_enum in cls:
+      if supported_lang_enum.name == lang_str:
+        return supported_lang_enum
+
+    raise ValueError(f'Unknown lang setting={lang_str}!')
+
+
+class ModelA(Protocol):
+  """Model A interface."""
+
+  def __init__(self, lang: LangEnum = LangEnum.en):
+    self.lang = lang
+
+  @property
+  def name(self) -> str:
+    """Model/Approch name."""
+    pass
+
+  def live_2_text(
+      self,
+      record_time_sec: int = 5,
+      output_audio_file_path: str | None = None) -> Audio2TextData:
+    """Records and transform audio into text.
+
+    Args:
+      record_time_sec: Recording time in seconds.
+      output_audio_file_path: Output audio fie path.
+
+    Returns:
+      `Audio2TextData` with trasnformed text.
+    """
+    pass
+
+  def audio_2_text(self, audio_file_path: str) -> Audio2TextData:
+    """Turns audio of given file path into text.
+
+    Args:
+      audio_file_path: Audio file path to do audio to text transformation.
+
+    Returns:
+      `Audio2TextData` with trasnformed text.
+    """
+    pass
+
+
+class ModelAMetric(Protocol):
+  """Clz to calculate metric score of model A.
+
+  Attributes:
+    name: Name of metric.
+    lang: Language of text to calculate metric.
+  """
+  name: str = 'Unknown'
+  do_sort_reverse: bool = True
+
+  def __init__(self, lang: LangEnum):
+    self._lang = lang
+
+  @property
+  def lang(self) -> LangEnum:
+    """The supported language(s) of MUT (model under test)."""
+    return self._lang
+
+  def score(
+      self,
+      transformed_text: str,
+      ground_truth_text: str) -> float:
+    """Calculates the metric score.
+
+    Args:
+      transformed_text: The text transformed by model A.
+      ground_truth_text: The ground truth of text.
+
+    Returns:
+      The corresponding metric score.
+    """
+    pass
+
+
+class ModelC(Protocol):
+  """Model C interface."""
+
+  def __init__(self, lang: LangEnum = LangEnum.en):
+    self.lang = lang
+
+  @property
+  def name(self) -> str:
+    """Model/Approch name."""
+    pass
+
+  def speak_text(
+      self,
+      text: str) -> None:
+    """Speaks out the given text.
+
+    Args:
+      text: Text to speak.
+    """
+    pass
+
+  def text_2_audio(
+      self,
+      text: str,
+      output_audio_file_path: str) -> Text2AudioData:
+    """Transform given text into audio.
+
+    Args:
+      text: Text to transform.
+      output_audio_file_path: Output audio file path to hold the transformed
+        result as audio.
+
+    Returns:
+      `Text2AudioData` with trasnformed audio information.
+    """
+    pass
